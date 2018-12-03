6464b9c002aa7fe22560fa817117c53a24448952
sheepxiaozi <2239798581@qq.com>
2018-12-03 19:36:56 +0800

    简单的首页/详情页视图

--- my_blog/article/views.py
+++ my_blog/article/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from article.models import Article
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse("Hello World")
+
+
+def detail(request, my_args):
+    post = Article.objects.all()[int(my_args)]
+    data = "title = %s, category = %s, date_time = %s, content = %s"\
+           % (post.title, post.category, post.date_time, post.content)
+    return HttpResponse(data)
+
+
+

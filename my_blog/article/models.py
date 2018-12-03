c3ac14ebd6969c3ca0e5802504f6ccdfcfa53e1c
sheepxiaozi <2239798581@qq.com>
2018-12-03 16:31:20 +0800

    创建博客文章模型类及其迁移

--- my_blog/article/models.py
+++ my_blog/article/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=100)  # 博客题目
+    category = models.CharField(max_length=50, blank=True)  # 博客标签
+    date_time = models.DateTimeField(auto_now_add=True)  # 博客日期
+    content = models.TextField(blank=True, null=True)  # 博客文章正文
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        db_table = 'tb_article'  # 指明数据库表名
+        ordering = ['-date_time']  # 按时间下降排序
+
+

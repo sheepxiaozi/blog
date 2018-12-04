46a66092657b88667639b8a8737dcec0623e7a74
sheepxiaozi <2239798581@qq.com>
2018-12-04 10:21:21 +0800

    动态url实现

--- my_blog/article/urls.py
+++ my_blog/article/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^index/$', views.index),
+    url(r'^index/$', views.index, name="index"),
+    url(r'^detail/(?P<id>\d+)/$', views.detail, name="detail"),
 ]
 
--- my_blog/article/views.py
+++ my_blog/article/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from article.models import Article
+from django.http import Http404
 
 
 def index(request):
     post_list = Article.objects.all()
     return render(request, "index.html", {"post_list": post_list})
+
+
+def detail(request, id):
+    try:
+        post = Article.objects.get(id=str(id))
+    except Article.DoesNotExist:
+        raise Http404
+    return render(request, 'post.html', {'post': post})
 
 

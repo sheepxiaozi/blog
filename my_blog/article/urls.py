f1e7111c99d46e27fd63b8b761632a872c77300e
sheepxiaozi <2239798581@qq.com>
2018-12-05 10:16:14 +0800

    归档和about_me页面展示

--- my_blog/article/urls.py
+++ my_blog/article/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     url(r'^index/$', views.index, name="index"),
     url(r'^detail/(?P<pk>\d+)/$', views.detail, name="detail"),
+    url(r'^archives/$', views.archives, name='archives'),
+    url(r'^about_me/$', views.about_me, name='about_me'),
+    url(r'^tag(?P<tag>\w+)/$', views.search_tag, name='search_tag'),
 ]
 
--- my_blog/article/views.py
+++ my_blog/article/views.py
@@ -16,3 +16,23 @@ def detail(request, pk):
     return render(request, 'post.html', {'post': post})
 
 
+def archives(request):
+    try:
+        post_list = Article.objects.all()
+    except Article.DoesNotExist:
+        raise Http404
+    return render(request, 'archives.html', {'post_list': post_list, 'error': False})
+
+
+def about_me(request):
+    return render(request, 'aboutme.html')
+
+
+def search_tag(request, tag):
+    try:
+        post_list = Article.objects.filter(category__iexact=tag)
+    except Article.DoesNotExist:
+        raise Http404
+    return render(request, 'tag.html', {'post_list': post_list})
+
+
